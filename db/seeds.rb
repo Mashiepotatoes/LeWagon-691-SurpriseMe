7d537498a4cb6755736f26c00d9f7105534cad42
Jeremy Lim <limjeremy29@gmail.com>
2021-09-22 13:14:35 +0800

    seed products and category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Xiaomi Mi Vacuum Cleaner Mini", price: 69.90)
+Product.create(name: "Jabra Elite 3 Earbuds", price: 128)
+Product.create(name: "Logitech 960-001090 C922 Pro Stream Webcam", price: 199)
+Product.create(name: "Adjustable Laptop Stand Oli & Ode Table Stand Portable Ergonomic Notebook Stand with Heat-Vent, Heavy Duty Laptop Holder Compatible with MacBook Pro/Air, Dell, HP, other laptop", price: 29.50)
+Product.create(name: "STOGA 60% Gaming Keyboard, RGB Mechanical Keyboard, Small 61-Key Wired Brown Switch Mini Keyboard, White Computer Keyboard for Gaming/Office/PC/Mac", price: 56.99)
+Product.create(name: "Apple AirPods Pro", price: 280.00)
+Product.create(name: "Dreamegg Cool Mist Humidifier - Ultrasonic Humidifiers for Bedroom, Baby & Home, Whisper Quiet Filter-Free Air Humidifier with 360° Mist Nozzle, Timer Setting, Night Light Option & Auto Shut-Off", price: 59.92)
+Product.create(name: "NYX PROFESSIONAL MAKEUP Ultimate Shadow Palette, Eyeshadow Palette, Warm Neutrals", price: 28.60)
+Product.create(name: "Expandable Cat Carrier Backpack for Cats, Space Capsule Bubble Pet Travel Carrier for Small Dog, Pet Hiking Traveling Backpack (Green, Expandable Backpack - Solid Hard)", price: 137.19)
+Product.create(name: "LONDONTOWN kur Nail Hardener and Base Coat", price: 30.00)
+
+Category.create(name: "Electronics")
+Category.create(name: "Beauty")
+Category.create(name: "Pets")
+Category.create(name: "Home")
+Category.create(name: "Gaming")
