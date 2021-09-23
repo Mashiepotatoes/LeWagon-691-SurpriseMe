65dcc3d77150ab61c909cfacebe6e95562b7d855
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-09-23 15:38:08 +0800

    add seeds from five categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,41 +21,70 @@ Category.destroy_all
 # Product.create(name: "Dreamegg Cool Mist Humidifier - Ultrasonic Humidifiers for Bedroom, Baby & Home, Whisper Quiet Filter-Free Air Humidifier with 360° Mist Nozzle, Timer Setting, Night Light Option & Auto Shut-Off", price: 59.92, description: "OPERATES RELIABLY IN SILENCE - Noise level is ≤30dB. No buzzing, hissing, or humming. This humidifier steadily and efficiently dispenses the soothing cool mist you need. 100,000+ happy users sleep better with Dreamegg. CONTROL AT YOUR FINGERTIPS - Designed with super simple intuitive interface & buttons. 360° mist nozzle: easily direct the flow of mist away from areas that don't need moisture and toward the areas that do. Easy to set up 1h/3h/6h timer before ready for bed.")
 # Product.create(name: "NYX PROFESSIONAL MAKEUP Ultimate Shadow Palette, Eyeshadow Palette, Warm Neutrals", price: 28.60, description: "EYESHADOW PALETTE: This professional level makeup palette features 16 highly-pigmented shadows that glide onto lids & make eyes pop with color. Use with NYX Professional makeup eyeshadow primer for bolder, longer-lasting color. ULTIMATE EYE SHADOW: Each high quality eyeshadow palette features a highly pigmented mix of finishes from matte & satin to shimmery & metallic—perfect for your skin tone. Apply primer & build your look. BOLD & BEAUTIFUL: Eyes pop with NYX PROFESSIONAL MAKEUP eye shadow collection. Choose from eyeshadow primer, matte eyeshadow, cream eyeshadows, gel eyeshadows, liquid eyeshadows, & more with a variety of different finishes to complete your look.")
 # Product.create(name: "Expandable Cat Carrier Backpack for Cats, Space Capsule Bubble Pet Travel Carrier for Small Dog, Pet Hiking Traveling Backpack (Green, Expandable Backpack - Solid Hard)", price:137.19, description: "Expandable Bubble Backpack for Cat: Wider visual view from the bubble dome. Expandable back panel to give more room and play time at outdoor activity. Breathable and Ventilation: Extra 9 holes around this pet carrier and two side windows to perform great air circulation and ventilation inside. Expanded back panel to be a giant tent bed to enjoy outdoor time with nature and sunshine well without feeling stuffiness in the backpack. Order the extra floral vent cover to get ever ventilation. Eye-catching and Lightweight: Entire backpack is less than 3 lbs only, suggest for cats from 0-12lbs and small dogs from 0-10lbs to leave enough room inside to turn around. Airline Approved backpack for cats and puppy. Super wear-resistant shell perfectly against biting and scratching by pets. Portable and Shoulder-Wear: Padded shoulder straps pet carrier backpack with adjustable chest buckle for travel and hiking outdoor activity")
-# Product.create(name: "LONDONTOWN kur Nail Hardener and Base Coat", price: 30.00, description: "Works to penetrate and replenish deteriorated nail protein and balance natural oils in the nail to strengthen and nourish them back to health. Created with Britain's rapeseed flower oil as well as a unique blend of natural extracts and vitamins to rejuvenate and restore moisture to dry, thin and brittle nails. Sets a strong base to nails. Vegan, gluten-free and cruelty-free. 5-free formula, formaldehyde-free, toluene-free, DBP-free, formaldehyde resin-free, camphor free.")
+# Product.create(name: "LONDONTOWN kur Nail Hardener and Base Coat", price: 30.00, description: "Works to penetrate and replenish deteriorated nail protein and balance natural oils in the nail to strengthen and nourish them back to health. Created with Britain's rapeseed flower oil as well as a unique blend of natural extracts and vitamins to rejuvenate and restore moisture to dry, thin and brittle nails. Sts a strong base to nails. Vegan, gluten-free and cruelty-free. 5-free formula, formaldehyde-free, toluene-free, DBP-free, formaldehyde resin-free, camphor free.")
 
-# Category.create(name: "Electronics")
-# Category.create(name: "Beauty & Cosmetics")
-# Category.create(name: "Pet Essentials")
-# Category.create(name: "Home & Lifestyle")
-# Category.create(name: "Office Accessories")
-beauty = Category.create(name: "Beauty General")
+# electronics = Category.create(name: "Electronics")
+all_beauty = Category.create(name: "Beauty & Cosmetics")
+video_games = Category.create(name: "Video Games")
+cd_vinyl = Category.create(name: "CD & Vinyl Records")
+pet_essentials = Category.create(name: "Pet Essentials")
+toys_games = Category.create(name: "Toys & Games")
+# home_lifestyle = Category.create(name: "Home & Lifestyle")
 
-# ---- Amazon All-Beauty Seeds ---- #
-file_path = File.join(__dir__, "amazon_datasets/all_beauty.json")
-serialised_All_beauty = File.read(file_path)
-parsed_all_beauty = JSON.parse(serialised_All_beauty)
+# ---- Amazon all_beauty Seeds ---- #
+file_path_beauty = File.join(__dir__, "amazon_datasets/all_beauty.json")
+serialised_all_beauty = File.read(file_path_beauty)
+parsed_all_beauty = JSON.parse(serialised_all_beauty)
 
-parsed_all_beauty.each do |product| puts 
-  name = product["title"]
-  p name
-  description = product["description"].join
-  p description
-  asin = product["asin"] 
-  p asin
-  image_url = product["image_url"]
-  p image_url
-  brand = product["brand"]
-  p brand
-  price = product["price"]
-  p price
+# ---- Amazon video_games seeds ---- #
+file_path_vg = File.join(__dir__, "amazon_datasets/video_games.json")
+serialised_video_games = File.read(file_path_vg)
+parsed_video_games = JSON.parse(serialised_video_games)
 
-  Product.new(name: name, description: description, price: price.to_s, asin: asin, image_url: image_url, brand: brand )
-  Product.category = beauty
-  Product.save
-  puts "#{product["title"]} saved"
+# # ---- Amazon cd_vinyl seeds ---- #
+# file_path_cdv = File.join(__dir__, "amazon_datasets/cd_vinyl.json")
+# serialised_cd_vinyl = File.read(file_path_cdv)
+# parsed_cd_vinyl = JSON.parse(serialised_cd_vinyl)
+
+# # ---- Amazon pet_supplies seeds ---- #
+# file_path_ps = File.join(__dir__, "amazon_datasets/pet_supplies.json")
+# serialised_pet_supplies = File.read(file_path_ps)
+# parsed_pet_supplies = JSON.parse(serialised_pet_supplies)
+
+# # ---- Amazon toys_games seeds ---- #
+# file_path_tg = File.join(__dir__, "amazon_datasets/toys_games.json")
+# serialised_toys_games = File.read(file_path_tg)
+# parsed_toys_games = JSON.parse(serialised_toys_games)
+
+# ---- seeding ---- #
+parsed_datasets = [parsed_all_beauty, parsed_video_games] 
+# , parsed_cd_vinyl, parsed_pet_supplies, parsed_toys_games]
+categories = [all_beauty, video_games] 
+# , cd_vinyl, pet_essentials, toys_games]
+categories_index = 0
+
+parsed_datasets.each do |dataset| 
+  dataset.each do |product|
+    name = product["title"]
+    description = product["description"].join
+    asin = product["asin"] # product number
+    image_url = product["imageUrlHighRes"]
+    brand = product["brand"]
+    price = product["price"]
+    
+    product = Product.new(name: name, description: description, price: price.to_s, asin: asin, image_url: image_url, brand: brand)
+    product.category = categories[categories_index]
+    binding.pry
+    product.save
+    puts "#{product["title"]} saved"
+  end
+  categories_index += 1
 end
 
 puts "Finished seeding"
-# Create products for product details (use existing data) - 3pm
+
 # fake the reviews (and ratings)
-# seeded user
+
+# ---- Create Users ---- #
+
+User.create(name:)
