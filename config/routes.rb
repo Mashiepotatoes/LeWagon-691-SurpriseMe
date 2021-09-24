fdda9416a9e294c73c5da7ea5a1ae5c198c72769
Pei Ern Koh <peiern.28@gmail.com>
2021-09-24 15:14:41 +0800

    stash

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :friendships, only: [:index, :show, :destroy, :create]
 
   post "/friendships/accept", to: 'friendships#accept'
   get "/friendships/requests", to: 'friendships#requests'
   get "/friendships/search", to: 'friendships#search'
+
+  resources :friendships, only: [:index, :show, :destroy, :create]
 
   resources :sessions, only: [:new, :create, :show] do
     resources :gift_recommendations, only: [:index]
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_09_23_163113) do
+ActiveRecord::Schema.define(version: 2021_09_24_065121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
