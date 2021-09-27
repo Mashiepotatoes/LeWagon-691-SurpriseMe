278ace092d5a38f678b0f3db254edebf56b92ae0
Jeremy Lim <limjeremy29@gmail.com>
2021-09-27 14:07:59 +0800

    able to select gifts

--- app/javascript/controllers/check_all_checkboxes_controller.js
+++ app/javascript/controllers/check_all_checkboxes_controller.js
@@ -4,4 +4,18 @@ export default class extends Controller {
   connect() {
     console.log("hello from StimulusJS")
   }
+
+  static targets = ["checkbox", "check4box"]
+
+  checkAll() {
+    if (this.checkboxTarget.checked === true) {
+      this.check4boxTargets.forEach((box) => {
+        box.checked = true;
+      });
+    } else {
+      this.check4boxTargets.forEach((box) => {
+        box.checked = false;
+      });
+    }
+  }
 }
