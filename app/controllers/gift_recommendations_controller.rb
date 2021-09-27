f8dfb27dd52416ad82afe562c4f04da04fbb2ed7
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-09-27 14:22:55 +0800

    drop reviews table

--- app/controllers/gift_recommendations_controller.rb
+++ app/controllers/gift_recommendations_controller.rb
@@ -1,6 +1,4 @@
 class GiftRecommendationsController < ApplicationController
-  RECOMMENDATIONS_ARRAY = []
-
   def index
     @product = Product.first
   end
@@ -8,17 +6,5 @@ class GiftRecommendationsController < ApplicationController
   def new
     @session = Session.new
     @gift_recommendation.session = @session
-  end
-
-  def rate
-    RECOMMENDATIONS_ARRAY << {user_id: 1, item_id: 1}
-  end
-
-  def recommend
-t return 5
-  end
-
-  def save_recommendation
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_031654) do
+ActiveRecord::Schema.define(version: 2021_09_27_062131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -153,17 +153,6 @@ ActiveRecord::Schema.define(version: 2021_09_27_031654) do
     t.index ["user_id"], name: "index_response_sets_on_user_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "content"
-    t.integer "rating"
-    t.bigint "order_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["order_id"], name: "index_reviews_on_order_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -196,6 +185,4 @@ ActiveRecord::Schema.define(version: 2021_09_27_031654) do
   add_foreign_key "response_sets", "answers"
   add_foreign_key "response_sets", "questions"
   add_foreign_key "response_sets", "users"
-  add_foreign_key "reviews", "orders"
-  add_foreign_key "reviews", "users"
 end
