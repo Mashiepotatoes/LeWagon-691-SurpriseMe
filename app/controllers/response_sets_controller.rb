480de80ccbc4a1750009c6ba5ada1b32c3d489d0
Jeremy Lim <limjeremy29@gmail.com>
2021-09-29 10:33:42 +0800

    set up response-set-index-page with basic css

--- app/controllers/response_sets_controller.rb
+++ app/controllers/response_sets_controller.rb
@@ -1,5 +1,12 @@
 class ResponseSetsController < ApplicationController
   def index
-    @response_sets = Response_set.all
+    # List all question
+    @question = Question.all
+
+    # List all answers by current user
+    # @response = ResponseSet.new
+    # @response.question_id = @question.id
+    # @response.user_id = current_user.id
+    # @response.answer_id = @answer.id
   end
 end
