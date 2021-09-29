6cca339c8e0b494c98efd2fc45f95991413e6dfb
Jeremy Lim <limjeremy29@gmail.com>
2021-09-29 12:23:21 +0800

    linking take a quiz button to first question

--- app/controllers/response_sets_controller.rb
+++ app/controllers/response_sets_controller.rb
@@ -1,9 +1,5 @@
 class ResponseSetsController < ApplicationController
   def index
-    # List all question
     @response_sets = ResponseSet.where(user_id: current_user)
-    # List all answers by current user
-    # How do i retrieve current user response for a particular qn?
-
   end
 end
