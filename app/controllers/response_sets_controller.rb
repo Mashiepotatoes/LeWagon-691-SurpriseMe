627ee222bc178d28c016f60e183f1d45ed7f6940
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-09-30 12:06:09 +0800

    edit response set landing page

--- app/controllers/response_sets_controller.rb
+++ app/controllers/response_sets_controller.rb
@@ -3,6 +3,7 @@ class ResponseSetsController < ApplicationController
     @response_sets = ResponseSet.where(user: current_user)
     @question = Question.first
     @all_questions = Question.all
+    @response_sets.nil? || (@response_sets.count != @all_questions.count) ? @quiz_landing = true : @quiz_landing = false
   end
 
   # To be updated if edit function is needed
