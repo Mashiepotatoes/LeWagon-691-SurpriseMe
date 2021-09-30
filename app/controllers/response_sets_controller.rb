fd2042a15981257242773cd45674f8747d1d6bb9
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-09-30 11:02:58 +0800

    response set page display landing if not all questions answered

--- app/controllers/response_sets_controller.rb
+++ app/controllers/response_sets_controller.rb
@@ -2,6 +2,7 @@ class ResponseSetsController < ApplicationController
   def index
     @response_sets = ResponseSet.where(user: current_user)
     @question = Question.first
+    @all_questions = Question.all
   end
 
   # To be updated if edit function is needed
