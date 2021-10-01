9f506fe832a1d7d62ee72184a101e472518af02a
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-10-02 02:53:08 +0800

    fix exit preference route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,10 +5,17 @@ class QuestionsController < ApplicationController
   end
 
   def landing
-    if !params[:id].nil?
-      @question = Question.find(params[:id])
+    if params[:id].nil?
+      answered_questions_id = ResponseSet.where(user: current_user).distinct.pluck(:question_id)
+      @question = Question.where.not(id: answered_questions_id).sample
+      if @question.nil?
+        flash[:alert] = "You have answered all the questions already!"
+        redirect_to response_sets_path
+      else
+        @question
+      end
     else
-      @question = @ #find a sample id
+      @question = Question.find(params[:id])
     end
   end
 end
