753d3cf8208113b272cfbb446778f22896952c5e
Jeremy Lim <limjeremy29@gmail.com>
2021-10-01 12:29:40 +0800

    Update stripe_checkout_session_service.rb

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,8 +1,5 @@
 class StripeCheckoutSessionService
   def call(event)
-    puts "======================"
-    puts "Webhook hit"
-    puts "======================"
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
     order.cart.line_items.update_all(status: false)
