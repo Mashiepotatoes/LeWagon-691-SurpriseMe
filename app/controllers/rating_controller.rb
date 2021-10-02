0efd036def2d00baa47a67862e826224d07f6023
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-10-02 13:31:58 +0800

    fix ratings route, add flash alert to product rating page

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -2,24 +2,25 @@ class RatingController < ApplicationController
   def new
     @rating = Rating.new
     @product = Product.find(params[:product_id])
-
+    flash[:alert] = "Your product rating will only help us give you better recommendations and will not be visible to the gifter"
   end
 
   def create
     @rating = Rating.new(rating_params)
-    @product = Product.find(params[:product_id])
+    @product = Product.find(params[:rating].require(:product))
     @rating.product = @product
     @rating.user = current_user
     @rating.save!
 
     # average rating
-    @product_ratings = Rating.where(product_id: params[:product_id])
+    @product_ratings = Rating.where(product: @product)
     @average_rating = @product_ratings.map { |product| product["rating"] }.sum / @product_ratings.count
     @product.average_rating = @average_rating
     @product.save!
 
     if @rating.save! && @product.save!
       redirect_to received_path
+      flash[:alert] = "Thank you for rating the product"
     else
       flash[:alert] = "Something went wrong."
       render :new
