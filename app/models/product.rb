eefcb3f4d4017301cd5f14c73a6052a0115c9332
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-10-13 11:39:02 +0800

    fix disco recommendations

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,21 +18,21 @@ class Product < ApplicationRecord
   }
 
   scope :by_category, ->(user_preferences) {
-    where({subcategory: user_preferences})
+    where({ subcategory: user_preferences })
   }
 
   def self.curate(gift_session, disco_recommendations, user_preferences)
     # if we don't get any recommendations
-    # if disco_recommendations.nil?
-      Product.all.sample(5)
-    # else
-    #   # if user doesn't have enough preferences data
-    #   if (user_preferences.nil?) || (user_preferences.count <= 2)
-    #     Product.all.sample(5)
-    #   else
+    if disco_recommendations.nil?
+      Product.all.by_category(user_preferences).sample(5)
+    else
+      # if user doesn't have enough preferences data
+      if user_preferences.nil? || user_preferences.count <= 2
+        Product.all.sample(5)
+      else
         # if not get disco recommendation
-        # disco_recommendations.for_occasion(gift_session.occasion_id).by_category(user_preferences).less_than(gift_session.budget)
-  #     end
-  #   end
-  # end
+        disco_recommendations.for_occasion(gift_session.occasion_id).by_category(user_preferences).less_than(gift_session.budget)
+      end
+    end
+  end
 end
--- lib/tasks/setup_disco.rake
+++ lib/tasks/setup_disco.rake
@@ -1,6 +1,7 @@
 namespace :setup_disco do
   task fit: :environment do
     puts "fitting data to disco"
+    # take all the ratings available in the database and extract user, item and rating and create dataset
     data = Rating.all.map do |rating|
       {
         user_id: rating.user.id,
@@ -8,12 +9,14 @@ namespace :setup_disco do
         rating: rating.rating
       }
     end
+    # create the recommender and fit it with the data set.
     recommender = Disco::Recommender.new
     recommender.fit(data)
 
+    # for each user, save their recommendations
     User.find_each do |user|
       p "saving recommendation for user: #{user.id}"
-      recs = recommender.user_recs(user.id)
+      recs = recommender.user_recs(user.id, count: 20)
       user.update_recommended_products(recs)
     end
   end
